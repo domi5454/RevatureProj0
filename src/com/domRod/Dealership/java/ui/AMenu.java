d6d838b8c6aba0ee0431d5e31f0380fe53211cf4
Domingo Rodriguez <domingorodriguez2012@gmail.com>
2021-03-06 16:34:46 -0600

    added employeeID method and UUID class

--- src/com/domRod/Dealership/java/ui/AMenu.java
+++ src/com/domRod/Dealership/java/ui/AMenu.java
@@ -5,11 +5,11 @@ public abstract class AMenu {
 
     public String greet = "********************************\n"+
             "Welcome to the Dom.Rev.Car.Showroom\n"+
-            "********************************\n\n\n\n"
+            "********************************\n\n\n"
             ;
     public String greet2 = "\n********************************\n"+
             "The Dom.Rev.Car.Showroom\n"+
-            "********************************\n\n\n\n";
+            "********************************\n\n\n";
 
 
 
